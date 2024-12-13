215f8ae59a028353024a703d8c6f17181d10723c
Pavel Barovsky <pbarovsky@mail.ru>
2024-12-13 12:59:19 +0300

    release1

---
+++ README.md
@@ -0,0 +1,82 @@
+# APX - Alias Package Manager
+
+APX is a command-line interface (CLI) package manager based on `apt`, specifically designed for Ubuntu-based distributions. It provides an intuitive alias system to simplify the process of managing packages.
+
+## Features
+- Simplifies APT commands with easy-to-remember aliases.
+- Covers essential operations like installing, removing, updating, and managing packages.
+- Built-in multilingual help system.
+
+---
+
+## Installation
+To install APX, run the following commands in your terminal:
+
+```bash
+chmod +x install_apx.sh
+./install_apx.sh
+```
+
+---
+
+## How It Works
+
+APX translates its commands into corresponding APT commands, making it easier to handle package management with shorter, user-friendly aliases.
+
+---
+
+## Available Commands
+
+| APX Command   | Description                               | Equivalent APT Command                  |
+|---------------|-------------------------------------------|------------------------------------------|
+| `apx i`       | Install a package                        | `sudo apt install <package>`            |
+| `apx r`       | Remove a package                         | `sudo apt remove <package>`             |
+| `apx rei`     | Reinstall a package                      | `sudo apt reinstall <package>`          |
+| `apx d`       | Download a package                       | `sudo apt download <package>`           |
+| `apx p`       | Purge a package                          | `sudo apt purge <package>`              |
+| `apx upd`     | Update package list                      | `sudo apt update`                       |
+| `apx upg`     | Upgrade installed packages               | `sudo apt upgrade`                      |
+| `apx fupg`    | Full upgrade                             | `sudo apt full-upgrade`                 |
+| `apx a`       | Remove unused packages                   | `sudo apt autoremove`                   |
+| `apx se`      | Search for packages                      | `apt search <search term>`              |
+| `apx l`       | List available or installed packages      | `apt list [<search term>]`              |
+| `apx sh`      | Show package information                 | `apt show <package>`                    |
+| `apx c`       | Clean the APT cache                      | `sudo apt clean`                        |
+| `apx check`   | Check the status of installed packages   | `sudo apt check`                        |
+| `apx hold`    | Hold a package to prevent updates        | `sudo apt-mark hold <package>`          |
+| `apx unhold`  | Unhold a package                         | `sudo apt-mark unhold <package>`        |
+| `apx policy`  | Display package version and priority     | `apt-cache policy <package>`            |
+
+---
+
+## GIF Demonstrations
+
+### Installing and removing package with APX
+![Installing Packages](./images/2.png)
+
+---
+
+## Help
+To view the help menu:
+
+- **English help**
+
+```bash
+apx en
+```
+
+- **Russian help**
+
+```bash
+apx ru
+```
+
+The help menu provides detailed information about all available commands.
+![Installing Packages](./images/1.png)
+
+---
+
+## Contributing
+Feel free to contribute by submitting pull requests or reporting issues in the GitHub repository. Your feedback and contributions are appreciated!
+
+---
---
+++ install_apx.sh
@@ -0,0 +1,127 @@
+#!/bin/bash
+
+ALIAS_CONTENT='
+# -------- Алиас для apx --------
+
+# Помощь на русском
+apx-ru() {
+  echo
+  echo -e "\033[1;33mapx - \033[1;37mМенеджер пакетов с интерфейсом командной строки на основе apt\033[0m"
+  echo -e "\033[1;32mИспользование:\033[0m apx {команда} [аргументы]"
+  echo
+  echo -e "\033[1;37mДоступные команды:\033[0m"
+  echo
+  
+  printf "\033[38;5;240m┌─────────────┬───────────────────────────────────────────┬──────────────────────────────────────┐\033[0m\n"
+  printf "\033[38;5;240m│ \033[1;33mКоманда apx\033[0m \033[38;5;240m│ \033[1;37mОписание\033[0m                                  \033[38;5;240m│ \033[1;38;5;240mКоманда apt\033[0m                          \033[38;5;240m│\033[0m\n"
+  printf "\033[38;5;240m├─────────────┼───────────────────────────────────────────┼──────────────────────────────────────┤\033[0m\n"
+
+  printf "\033[38;5;240m| \033[1;33mapx i\033[0m       \033[38;5;240m| \033[1;37mУстановка пакета                          \033[38;5;240m| \033[38;5;240msudo apt install <package name>\033[0m      \033[38;5;240m|\n"
+  printf "\033[38;5;240m| \033[1;33mapx r\033[0m       \033[38;5;240m| \033[1;37mУдаление пакета                           \033[38;5;240m| \033[38;5;240msudo apt remove <package name>\033[0m       \033[38;5;240m|\n"
+  printf "\033[38;5;240m| \033[1;33mapx rei\033[0m     \033[38;5;240m| \033[1;37mПереустановка пакета                      \033[38;5;240m| \033[38;5;240msudo apt reinstall <package name>\033[0m    \033[38;5;240m|\n"
+  printf "\033[38;5;240m| \033[1;33mapx d\033[0m       \033[38;5;240m| \033[1;37mЗагрузка пакета                           \033[38;5;240m| \033[38;5;240msudo apt download <package name>\033[0m     \033[38;5;240m|\n"
+  printf "\033[38;5;240m| \033[1;33mapx p\033[0m       \033[38;5;240m| \033[1;37mПолное удаление пакета                    \033[38;5;240m| \033[38;5;240msudo apt purge <package name>\033[0m        \033[38;5;240m|\n"
+  printf "\033[38;5;240m| \033[1;33mapx upd\033[0m     \033[38;5;240m| \033[1;37mОбновление списка пакетов                 \033[38;5;240m| \033[38;5;240msudo apt update\033[0m                      \033[38;5;240m|\n"
+  printf "\033[38;5;240m| \033[1;33mapx upg\033[0m     \033[38;5;240m| \033[1;37mОбновление установленных пакетов          \033[38;5;240m| \033[38;5;240msudo apt upgrade\033[0m                     \033[38;5;240m|\n"
+  printf "\033[38;5;240m| \033[1;33mapx fupg\033[0m    \033[38;5;240m| \033[1;37mПолное обновление                         \033[38;5;240m| \033[38;5;240msudo apt full-upgrade\033[0m                \033[38;5;240m|\n"
+  printf "\033[38;5;240m| \033[1;33mapx a\033[0m       \033[38;5;240m| \033[1;37mУдаление неиспользуемых пакетов           \033[38;5;240m| \033[38;5;240msudo apt autoremove\033[0m                  \033[38;5;240m|\n"
+  printf "\033[38;5;240m| \033[1;33mapx se\033[0m      \033[38;5;240m| \033[1;37mПоиск пакетов                             \033[38;5;240m| \033[38;5;240mapt search <search term>\033[0m             \033[38;5;240m|\n"
+  printf "\033[38;5;240m| \033[1;33mapx l\033[0m       \033[38;5;240m| \033[1;37mСписок пакетов                            \033[38;5;240m| \033[38;5;240mapt list [<search term>]\033[0m             \033[38;5;240m|\n"
+  printf "\033[38;5;240m| \033[1;33mapx sh\033[0m      \033[38;5;240m| \033[1;37mОтображение информации о пакете           \033[38;5;240m| \033[38;5;240mapt show <package name>\033[0m              \033[38;5;240m|\n"
+  printf "\033[38;5;240m| \033[1;33mapx c\033[0m       \033[38;5;240m| \033[1;37mОчистка кэша APT                          \033[38;5;240m| \033[38;5;240msudo apt clean\033[0m                       \033[38;5;240m|\n"
+  printf "\033[38;5;240m| \033[1;33mapx check\033[0m   \033[38;5;240m| \033[1;37mПроверка состояния установленных пакетов  \033[38;5;240m| \033[38;5;240msudo apt check\033[0m                       \033[38;5;240m|\n"
+  printf "\033[38;5;240m| \033[1;33mapx hold\033[0m    \033[38;5;240m| \033[1;37mУдержание пакета                          \033[38;5;240m| \033[38;5;240msudo apt-mark hold <package name>\033[0m    \033[38;5;240m|\n"
+  printf "\033[38;5;240m| \033[1;33mapx unhold\033[0m  \033[38;5;240m| \033[1;37mСнятие удержания с пакета                 \033[38;5;240m| \033[38;5;240msudo apt-mark unhold <package name>\033[0m  \033[38;5;240m|\n"
+  printf "\033[38;5;240m| \033[1;33mapx policy\033[0m  \033[38;5;240m| \033[1;37mОтображение политики пакета               \033[38;5;240m| \033[38;5;240mapt-cache policy <package name>\033[0m      \033[38;5;240m|\n"
+
+  printf "\033[38;5;240m└─────────────┴───────────────────────────────────────────┴──────────────────────────────────────┘\033[0m\n"
+  echo
+}
+
+
+# Помощь на английском
+apx-en() {
+  echo
+  echo -e "\033[1;33mapx - \033[1;37mPackage manager with command line interface based on apt\033[0m"
+  echo -e "\033[1;32mUsage:\033[0m apx {command} [arguments]"
+  echo
+  echo -e "\033[1;37mAvailable commands:\033[0m"
+  echo
+  
+  printf "\033[38;5;240m┌─────────────────┬────────────────────────────────────────┬──────────────────────────────────────┐\033[0m\n"
+  printf "\033[38;5;240m│ \033[1;33mThe apx command\033[0m \033[38;5;240m│ \033[1;37mDescription\033[0m                            \033[38;5;240m│ \033[1;38;5;240mThe apt command\033[0m                      \033[38;5;240m│\033[0m\n"
+  printf "\033[38;5;240m├─────────────────┼────────────────────────────────────────┼──────────────────────────────────────┤\033[0m\n"
+
+  printf "\033[38;5;240m| \033[1;33mapx i\033[0m           \033[38;5;240m| \033[1;37mInstall package                        \033[38;5;240m| \033[38;5;240msudo apt install <package name>\033[0m      \033[38;5;240m|\n"
+  printf "\033[38;5;240m| \033[1;33mapx r\033[0m           \033[38;5;240m| \033[1;37mRemove package                         \033[38;5;240m| \033[38;5;240msudo apt remove <package name>\033[0m       \033[38;5;240m|\n"
+  printf "\033[38;5;240m| \033[1;33mapx rei\033[0m         \033[38;5;240m| \033[1;37mReinstall package                      \033[38;5;240m| \033[38;5;240msudo apt reinstall <package name>\033[0m    \033[38;5;240m|\n"
+  printf "\033[38;5;240m| \033[1;33mapx d\033[0m           \033[38;5;240m| \033[1;37mDownload package                       \033[38;5;240m| \033[38;5;240msudo apt download <package name>\033[0m     \033[38;5;240m|\n"
+  printf "\033[38;5;240m| \033[1;33mapx p\033[0m           \033[38;5;240m| \033[1;37mPurge package                          \033[38;5;240m| \033[38;5;240msudo apt purge <package name>\033[0m        \033[38;5;240m|\n"
+  printf "\033[38;5;240m| \033[1;33mapx upd\033[0m         \033[38;5;240m| \033[1;37mUpdate package list                    \033[38;5;240m| \033[38;5;240msudo apt update\033[0m                      \033[38;5;240m|\n"
+  printf "\033[38;5;240m| \033[1;33mapx upg\033[0m         \033[38;5;240m| \033[1;37mUpgrade installed packages             \033[38;5;240m| \033[38;5;240msudo apt upgrade\033[0m                     \033[38;5;240m|\n"
+  printf "\033[38;5;240m| \033[1;33mapx fupg\033[0m        \033[38;5;240m| \033[1;37mFull upgrade                           \033[38;5;240m| \033[38;5;240msudo apt full-upgrade\033[0m                \033[38;5;240m|\n"
+  printf "\033[38;5;240m| \033[1;33mapx a\033[0m           \033[38;5;240m| \033[1;37mRemove unused packages                 \033[38;5;240m| \033[38;5;240msudo apt autoremove\033[0m                  \033[38;5;240m|\n"
+  printf "\033[38;5;240m| \033[1;33mapx se\033[0m          \033[38;5;240m| \033[1;37mSearch packages                        \033[38;5;240m| \033[38;5;240mapt search <search term>\033[0m             \033[38;5;240m|\n"
+  printf "\033[38;5;240m| \033[1;33mapx l\033[0m           \033[38;5;240m| \033[1;37mList packages                          \033[38;5;240m| \033[38;5;240mapt list [<search term>]\033[0m             \033[38;5;240m|\n"
+  printf "\033[38;5;240m| \033[1;33mapx sh\033[0m          \033[38;5;240m| \033[1;37mShow package information               \033[38;5;240m| \033[38;5;240mapt show <package name>\033[0m              \033[38;5;240m|\n"
+  printf "\033[38;5;240m| \033[1;33mapx c\033[0m           \033[38;5;240m| \033[1;37mClean APT cache                        \033[38;5;240m| \033[38;5;240msudo apt clean\033[0m                       \033[38;5;240m|\n"
+  printf "\033[38;5;240m| \033[1;33mapx check\033[0m       \033[38;5;240m| \033[1;37mChecking status of installed packages  \033[38;5;240m| \033[38;5;240msudo apt check\033[0m                       \033[38;5;240m|\n"
+  printf "\033[38;5;240m| \033[1;33mapx hold\033[0m        \033[38;5;240m| \033[1;37mHold package                           \033[38;5;240m| \033[38;5;240msudo apt-mark hold <package name>\033[0m    \033[38;5;240m|\n"
+  printf "\033[38;5;240m| \033[1;33mapx unhold\033[0m      \033[38;5;240m| \033[1;37mUnhold package                         \033[38;5;240m| \033[38;5;240msudo apt-mark unhold <package name>\033[0m  \033[38;5;240m|\n"
+  printf "\033[38;5;240m| \033[1;33mapx policy\033[0m      \033[38;5;240m| \033[1;37mShow package policy                    \033[38;5;240m| \033[38;5;240mapt-cache policy <package name>\033[0m      \033[38;5;240m|\n"
+
+  printf "\033[38;5;240m└─────────────────┴────────────────────────────────────────┴──────────────────────────────────────┘\033[0m\n"
+  echo
+}
+
+# Основная функция apx
+apx() {
+  case "$1" in
+    ru) apx-ru ;;
+    en) apx-en ;;
+    i) shift; sudo apt install "$@" ;;
+    r) shift; sudo apt remove "$@" ;;
+    rei) shift; sudo apt reinstall "$@" ;;
+    d) shift; sudo apt download "$@" ;;
+    p) shift; sudo apt purge "$@" ;;
+    upd) sudo apt update ;;
+    upg) sudo apt upgrade ;;
+    fupg) sudo apt full-upgrade ;;
+    a) sudo apt autoremove ;;
+    se) shift; apt search "$@" ;;
+    l) shift; apt list "$@" ;;
+    sh) shift; apt show "$@" ;;
+    c) sudo apt clean ;;
+    check) sudo apt check ;;
+    hold) shift; sudo apt-mark hold "$@" ;;
+    unhold) shift; sudo apt-mark unhold "$@" ;;
+    policy) shift; apt-cache policy "$@" ;;
+    *) 
+      echo -e "Enter the following command to view the apx help:"
+      echo -e "apx ru - Russian help"
+      echo -e "apx en - English help"
+  esac
+}
+'
+
+# Функция для добавления алиасов в .bashrc и .zshrc (если они существуют)
+add_aliases_to_shell_config() {
+  # Файлы конфигурации
+  local files=("$HOME/.bashrc" "$HOME/.zshrc")
+  
+  for file in "${files[@]}"; do
+    if [ -f "$file" ]; then
+      # Проверим, не добавлен ли уже алиас
+      if ! grep -q "apx-help" "$file"; then
+        echo -e "$ALIAS_CONTENT" >> "$file"
+        echo "Aliases have been successfully added to $file"
+      else
+        echo "Aliases have already been added to $file"
+      fi
+    fi
+  done
+}
+
+# Выполняем добавление алиасов
+add_aliases_to_shell_config
+
